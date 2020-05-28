66e5283db8508e810a8c5f63f65077f35267910d
srolandmarshall <samuel.r.marshall@gmail.com>
2020-05-28 00:34:13 -0400

    refactored use interval, noved to own file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Clock from "./Clock";
-
-function useInterval(callback, delay) {
-	const savedCallback = useRef();
-
-	// Remember the latest callback.
-	useEffect(() => {
-		savedCallback.current = callback;
-	}, [callback]);
-
-	// Set up the interval.
-	useEffect(() => {
-		function tick() {
-			savedCallback.current();
-		}
-		if (delay !== null) {
-			let id = setInterval(tick, delay);
-			return () => clearInterval(id);
-		}
-	}, [delay]);
-}
+import { useInterval } from "./useInterval";
 
 function App() {
 	const [timeString, setTimeString] = useState(new Date().toLocaleTimeString());
